e727cc80218d44023f91e8a8d77828318b393f4f
J27 <jean.d.marchal@gmail.com>
2016-08-12 09:34:23 +0200

    [FrequencyFit] add tests to test.R

--- tests/test.R
+++ tests/test.R
@@ -1,24 +1,81 @@
 library(SpaDES)
 
-mySim <- simInit(
-  times = list(start = 1, end = 1, timeunit = "year"),
-  modules = list("fireSense_FrequencyFit"),
-  paths = list(modulePath = " # replace with empty string instead"),
-  params = list(fireSense_FrequencyFit = list(
-    #formula = formula(NB_FIRES_L ~ MDC_JUL * HW),
-#    formula = formula(NB_FIRES_L ~ pw(MDC_JUL, K) * HW),
-    formula = formula(NB_FIRES_L ~ MDC_JUL:HW + MDC_JUL:CN + MDC_JUL:DIST + MDC_JUL:O +
-                     pw(MDC_JUL, BP1):HW + pw(MDC_JUL, BP2):CN + pw(MDC_JUL, BP3):DIST + pw(MDC_JUL, BP4):O - 1),
-    family = "negative.binomial",
-    trace = 5,
-    data = "dataFireSense_FrequencyFit"
-  )),
-  inputs = data.frame(
-    files = "Z:/dataFireSense_FrequencyFit.rds",
-    objectName = "dataFireSense_FrequencyFit",
-    functions = "readRDS",
-    package = "base",
-    stringsAsFactors = FALSE)
-  )
+## Not piecewise
+  ## Poisson
+    # mySim <- simInit(
+    #   times = list(start = 1, end = 1, timeunit = "year"),
+    #   modules = list("fireSense_FrequencyFit"),
+    #   paths = list(modulePath = " # replace with empty string instead"),
+    #   params = list(fireSense_FrequencyFit = list(
+    #     formula = formula(NB_FIRES_L ~ MDC_JUL * HW),
+    #     family = "poisson",
+    #     trace = 5,
+    #     data = "dataFireSense_FrequencyFit")
+    #   ),
+    #   inputs = data.frame(
+    #     files = "Z:/dataFireSense_FrequencyFit.rds",
+    #     objectName = "dataFireSense_FrequencyFit",
+    #     functions = "readRDS",
+    #     package = "base",
+    #     stringsAsFactors = FALSE)
+    #   )
+
+  ## Negative binomial  
+    # mySim <- simInit(
+    #   times = list(start = 1, end = 1, timeunit = "year"),
+    #   modules = list("fireSense_FrequencyFit"),
+    #   paths = list(modulePath = " # replace with empty string instead"),
+    #   params = list(fireSense_FrequencyFit = list(
+    #     formula = formula(NB_FIRES_L ~ MDC_JUL * HW),
+    #     trace = 5,
+    #     data = "dataFireSense_FrequencyFit")
+    #   ),
+    #   inputs = data.frame(
+    #     files = "Z:/dataFireSense_FrequencyFit.rds",
+    #     objectName = "dataFireSense_FrequencyFit",
+    #     functions = "readRDS",
+    #     package = "base",
+    #     stringsAsFactors = FALSE)
+    #   )
+
+# Piecewise
+  ## Poisson
+    # mySim <- simInit(
+    #   times = list(start = 1, end = 1, timeunit = "year"),
+    #   modules = list("fireSense_FrequencyFit"),
+    #   paths = list(modulePath = " # replace with empty string instead"),
+    #   params = list(fireSense_FrequencyFit = list(
+    #     formula = formula(NB_FIRES_L ~ MDC_JUL:HW + MDC_JUL:CN + MDC_JUL:DIST + MDC_JUL:O +
+    #                         pw(MDC_JUL, K1):HW + pw(MDC_JUL, K2):CN + pw(MDC_JUL, K3):DIST + pw(MDC_JUL, K4):O - 1),
+    #     family = "poisson",
+    #     trace = 5,
+    #     data = "dataFireSense_FrequencyFit")
+    #   ),
+    #   inputs = data.frame(
+    #     files = "Z:/dataFireSense_FrequencyFit.rds",
+    #     objectName = "dataFireSense_FrequencyFit",
+    #     functions = "readRDS",
+    #     package = "base",
+    #     stringsAsFactors = FALSE)
+    # )
+  
+  ## Negative binomial
+    mySim <- simInit(
+      times = list(start = 1, end = 1, timeunit = "year"),
+      modules = list("fireSense_FrequencyFit"),
+      paths = list(modulePath = " # replace with empty string instead"),
+      params = list(fireSense_FrequencyFit = list(
+        formula = formula(NB_FIRES_L ~ MDC_JUL:HW + MDC_JUL:CN + MDC_JUL:DIST + MDC_JUL:O +
+                          pw(MDC_JUL, K1):HW + pw(MDC_JUL, K2):CN + pw(MDC_JUL, K3):DIST + pw(MDC_JUL, K4):O - 1),
+        trace = 5,
+        data = "dataFireSense_FrequencyFit")
+      ),
+      inputs = data.frame(
+        files = "Z:/dataFireSense_FrequencyFit.rds",
+        objectName = "dataFireSense_FrequencyFit",
+        functions = "readRDS",
+        package = "base",
+        stringsAsFactors = FALSE)
+    )
 
 spades(mySim)
