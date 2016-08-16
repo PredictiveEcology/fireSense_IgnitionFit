a4941efdaa14c8a794a73ace44046ddf1b57a7ff
J27 <jean.d.marchal@gmail.com>
2016-08-16 17:00:12 +0200

    "DIST" ->"D"

--- tests/test.R
+++ tests/test.R
@@ -45,8 +45,8 @@ library(SpaDES)
     #   modules = list("fireSense_FrequencyFit"),
     #   paths = list(modulePath = " # replace with empty string instead"),
     #   params = list(fireSense_FrequencyFit = list(
-    #     formula = formula(NB_FIRES_L ~ MDC_JUL:HW + MDC_JUL:CN + MDC_JUL:DIST + MDC_JUL:O +
-    #                         pw(MDC_JUL, K1):HW + pw(MDC_JUL, K2):CN + pw(MDC_JUL, K3):DIST + pw(MDC_JUL, K4):O - 1),
+    #     formula = formula(NB_FIRES_L ~ MDC_JUL:HW + MDC_JUL:CN + MDC_JUL:D + MDC_JUL:O +
+    #                         pw(MDC_JUL, K1):HW + pw(MDC_JUL, K2):CN + pw(MDC_JUL, K3):D + pw(MDC_JUL, K4):O - 1),
     #     family = "poisson",
     #     trace = 5,
     #     data = "dataFireSense_Frequency")
@@ -65,8 +65,8 @@ library(SpaDES)
     #   modules = list("fireSense_FrequencyFit"),
     #   paths = list(modulePath = " # replace with empty string instead"),
     #   params = list(fireSense_FrequencyFit = list(
-    #     formula = formula(NB_FIRES_L ~ MDC_JUL:HW + MDC_JUL:CN + MDC_JUL:DIST + MDC_JUL:O +
-    #                       pw(MDC_JUL, K1):HW + pw(MDC_JUL, K2):CN + pw(MDC_JUL, K3):DIST + pw(MDC_JUL, K4):O - 1),
+    #     formula = formula(NB_FIRES_L ~ MDC_JUL:HW + MDC_JUL:CN + MDC_JUL:D + MDC_JUL:O +
+    #                       pw(MDC_JUL, K1):HW + pw(MDC_JUL, K2):CN + pw(MDC_JUL, K3):D + pw(MDC_JUL, K4):O - 1),
     #     family = MASS::negative.binomial(theta = 1, link = identity),
     #     ub = list(beta = rep(1e-1, 8)),
     #     trace = 5,
