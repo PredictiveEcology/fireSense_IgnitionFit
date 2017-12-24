f4cb358c2617a50efa9b010c4a066f1ddc327a46
JM <jean.d.marchal@gmail.com>
2017-12-23 22:18:42 -0500

    Reproducible paths for test / examples

--- tests/Test_FrequencyFit.R
+++ tests/Test_FrequencyFit.R
@@ -2,7 +2,7 @@ library(SpaDES)
 
 ## TODO: Expliquer que les coefficients du trace sont scalés et également que les bounds doivent être scalées (ou modif code).
 
-modulePath <- "~/Documents/GitHub/McIntire-lab/modulesPrivate/"
+modulePath <- normalizePath("..")
 
 # Define simulation parameters
 times <- list(start = 1, end = 1, timeunit = "year")
@@ -22,7 +22,6 @@ parameters <- list(
     formula = formula,
     family = quote(MASS::negative.binomial(theta = 1, link = "identity")),
     ub = list(coef = c(1, 1, 1, 1, 1, 1, 1, 1)),
-    # start = c(1, 1),
     data = "dataFireSense_FrequencyFit",
     trace = 10, # Print progress every 10 iterations
     itermax = 10,
@@ -34,7 +33,7 @@ parameters <- list(
 # Define from where and how data will be loaded in the simList environment
 inputs <- data.frame(
   objectName = "dataFireSense_FrequencyFit",
-  file = "C:/Z/Contrats/Pessiere/DataInputs/dataFireSense_FrequencyFit_foudre.rds",
+  file = normalizePath("../inputs/dataFireSense_FrequencyFit.rds"),
   fun = "readRDS",
   package = "base",
   loadTime = 1
